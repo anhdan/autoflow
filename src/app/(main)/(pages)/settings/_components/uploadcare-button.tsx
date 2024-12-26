e85429ceb262b844186736ba56fc9aac1e5b99c1
martin <martin@martins-MacBook-Air.local>
2024-12-26 13:08:31 +0700

    fixing avatar

--- src/app/(main)/(pages)/settings/_components/uploadcare-button.tsx
+++ src/app/(main)/(pages)/settings/_components/uploadcare-button.tsx
@@ -1,21 +1,39 @@
 'use client'
 
-import React from 'react'
-import { FileUploaderRegular } from '@uploadcare/react-uploader/next';
+import React, { useEffect, useRef } from 'react'
+import { FileUploaderRegular } from '@uploadcare/react-uploader'
 import '@uploadcare/react-uploader/core.css';
+import { useRouter } from 'next/navigation';
 
 type Props = {
-    onUpload?: any 
+    onUpload: (e: string) => any
 }
 
-function UploadCareButton({}: Props) {
+function UploadCareButton({ onUpload }: Props) {
+
+  const router = useRouter()
+
+  useEffect(() => {
+    console.log('✅', 'Hello uploadcare')
+    const handleUpload = async (e: any) => {
+      const file = await onUpload(e.detail.cdnUrl)
+      if (file) {
+        router.refresh()
+      }
+    }
+    document.addEventListener('file-upload-success', handleUpload)
+  }, [])
+
   return (
     <div>
         <FileUploaderRegular
          sourceList="local, url, camera, dropbox"
          classNameUploader="uc-light"
          pubkey="c525aa1eafccf7e28ee0"
+         onFileUploadSuccess={(e) => console.log('✅', e)}
       />
+
+      
     </div>
   )
 }
--- src/app/(main)/(pages)/settings/page.tsx
+++ src/app/(main)/(pages)/settings/page.tsx
@@ -2,12 +2,46 @@ import ProfileForm from '@/components/forms/profile-form'
 import React from 'react'
 import ProfilePicture from './_components/profile-picture'
 import { db } from '@/lib/db'
-// import { currentUser } from '@clerk/nextjs'
+import { currentUser } from '@clerk/nextjs/server'
 
 type Props = {}
 
-const Settings = (props: Props) => {
+const Settings = async (props: Props) => {
+    const authUser = await currentUser()
+    if (!authUser) return null
+
+    const user = await db.user.findUnique({ where: { clerkId: authUser.id } })
+
     // WIP: Wire up profile picture
+    const removeProfileImage = async () => {
+        'use server'
+        const response = await db.user.update({
+            where: {
+                clerkId: authUser.id,
+            },
+            data: {
+                profileImage: '',
+            },
+        })
+        return response
+    }
+
+    const uploadProfileImage = async (image: string) => {
+        'use server'
+        const id = authUser.id
+        console.log('🚀', id)
+        const response = await db.user.update({
+            where: {
+                clerkId: id,
+            },
+            data: {
+                profileImage: image,
+            },
+        })
+
+        return response
+    }
+
     return (
         <div className='flex flex-col gap-4'>
             <h1 className="sticky top-0 z-[10] flex items-center justify-between border-b bg-background/50 p-6 text-4xl backdrop-blur-lg">
@@ -22,7 +56,12 @@ const Settings = (props: Props) => {
                     </p>
                 </div>
 
-                {/* <ProfilePicture /> */}
+                <ProfilePicture
+                    onDelete={removeProfileImage}
+                    userImage={user?.profileImage || ''}
+                    onUpload={uploadProfileImage}
+                >
+                </ProfilePicture>
 
                 <ProfileForm />
             </div>
